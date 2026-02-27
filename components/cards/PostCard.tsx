8aa4dcb91e813a489c9966d5f7d3bf9c32d94102
Chukwubuikem-Onwuchuruba <ikemonwuchuruba2004@gmail.com>
2026-02-27 14:47:06 -0600

    fixing the createdAt on the frontend

--- components/cards/PostCard.tsx
+++ components/cards/PostCard.tsx
@@ -153,25 +153,29 @@ function PostCard({
         </div>
       )}
 
-      {!isComment && community && (
-        <Link
-          href={`/communities/${community.id}`}
-          className="mt-5 flex items-center"
-        >
+      <div className="mt-5 flex items-center">
+        {community ? (
+          <Link
+            href={`/communities/${community.id}`}
+            className="flex items-center"
+          >
+            <p className="text-subtle-medium text-gray-1">
+              {formatDateString(createdAt)} - {community.name} Community
+            </p>
+            <Image
+              src={community.image}
+              alt={community.name}
+              width={14}
+              height={14}
+              className="ml-1 rounded-full object-cover"
+            />
+          </Link>
+        ) : (
           <p className="text-subtle-medium text-gray-1">
             {formatDateString(createdAt)}
-            {community && ` - ${community.name} Community`}
           </p>
-
-          <Image
-            src={community.image}
-            alt={community.name}
-            width={14}
-            height={14}
-            className="ml-1 rounded-full object-cover"
-          />
-        </Link>
-      )}
+        )}
+      </div>
     </article>
   );
 }
